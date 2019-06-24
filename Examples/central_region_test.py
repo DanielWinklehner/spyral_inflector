35e58e6e8ad8c6310f3f3f84a16db82f42382852
pweigel <plw38@drexel.edu>
2019-06-24 17:36:02 -0400

    Some more CR functionality

--- Examples/central_region_test.py
+++ Examples/central_region_test.py
@@ -1,8 +1,8 @@
 from spyral_inflector import *
 import numpy as np
 
-# h2p = IonSpecies("H2_1+", 0.035)
-# h2p.calculate_from_energy_mev(0.07 / h2p.a())
+h2p = IonSpecies("H2_1+", 0.035)
+h2p.calculate_from_energy_mev(0.07 / h2p.a())
 #
 # si = SpiralInflector(ion=h2p,
 #                      method="numerical",
@@ -26,8 +26,38 @@ import numpy as np
 #
 # si.generate_geometry()
 
-cr = CentralRegion()
-# cr.initialize()
+cr = CentralRegion(r_cr=0.5)
+cr.initialize(xi=np.array([0.05, 0.0, 0.0]),
+              vi=np.array([0.0, h2p.v_m_per_s(), 0.0]))
+
+my_dee = Dee()
+my_dee.initialize()
+
+my_second_dee = Dee()
+my_second_dee.initialize()
+my_second_dee.rotate(90, angle_unit="deg")
+
+my_third_dee = Dee()
+my_third_dee.initialize()
+my_third_dee.rotate(180, angle_unit="deg")
+
+my_fourth_dee = Dee()
+my_fourth_dee.initialize()
+my_fourth_dee.rotate(270, angle_unit="deg")
+
+cr.add_dee(my_dee)
+cr.add_dee(my_second_dee)
+cr.add_dee(my_third_dee)
+cr.add_dee(my_fourth_dee)
+cr.plot_dees()
+
+thetas = [10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0]
+
+for theta in thetas:
+    for dee in cr._dees:
+        dee.next_top_segment(angle_offset=theta)
+        dee.next_bottom_segment(angle_offset=theta)
+cr.plot_dees()
 
 # cr.load_bfield(bfield='/home/philip/Downloads/RFQ-DIP_TestCyclotron_MainField.table',
 #                bf_scale=1E-4,
@@ -35,5 +65,5 @@ cr = CentralRegion()
 #                extents=np.array([[-30.0, 30.0], [-30.0, 30.0], [-2.0, 2.0]]),
 #                extents_dims=["X", "Y", "Z"])
 
-cr.load_bfield(bfield=Field(dim=0, field={"x": 0.0, "y": 0.0, "z": -1.04}))
+# cr.load_bfield(bfield=Field(dim=0, field={"x": 0.0, "y": 0.0, "z": -1.04}))
 
--- spyral_inflector/central_region.py
+++ spyral_inflector/central_region.py
@@ -12,14 +12,8 @@ from .trajectories import *
 colors = MyColors()
 
 
-class Dee(object):
-    def __init__(self, dummy=False, voltage=7.0E4):
-        self.dummy = dummy
-        self.voltage = voltage
-
-
 class CentralRegion(object):
-    def __init__(self, spiral_inflector=None):
+    def __init__(self, spiral_inflector=None, r_cr=0.3):
 
         self._initialized = False
 
@@ -29,15 +23,17 @@ class CentralRegion(object):
         self._variables_analytic = {}
         self._variables_numerical = {}
         self._variables_track = {}
-        self._segments = []
+
+        self._dees = []
 
         self._tracked_trjs = []
         self._track_trjs_vel = []
 
         self.rf_phase = 0.0
-        self.rf_frequency = 32.8E6
+        self.rf_freq = 32.8E6
         self.harmonic = 4
         self.v_peak = 70.0E3
+        self.r_cr = r_cr  # Radius at which we no longer consider the central region as a different entity
 
         if spiral_inflector is not None:
             self.set_inflector(spiral_inflector)
@@ -76,47 +72,33 @@ class CentralRegion(object):
     def track_variables(self, track_variables):
         self._variables_track = track_variables
 
-    def dee_crossing(self, rc, rd, v, t):
-        ion = self._params_analytic["ion"]
-        initial_v = ion.v_m_per_s()
+    # def dee_crossing(self, rc, rd, v, t):
+    #     ion = self._params_analytic["ion"]
+    #     initial_v = ion.v_m_per_s()
+    #
+    #     dE = 0.0
+    #     ttf = 1.0
+    #
+    #     for segment in self._segments:
+    #         if segment.check_intersection(rc[:2], rd[:2]):
+    #             print('Intersection')
+    #             dE += ion.q() * np.sin(2 * np.pi * self.rf_freq * t + self.rf_phase + segment.phase) * self.v_peak * ttf
+    #             print(np.mod(np.rad2deg(2 * np.pi * self.rf_freq * t + self.rf_phase), 360))
+    #
+    #     # if not intersected:
+    #     #     return v
+    #     # dE = ion.q() * np.sin( 2 * np.pi * self.rf_freq * t + self.rf_phase) * self.v_peak * ttf
+    #     # print("Gained energy {:.3f} keV".format(dE / 1E3))
+    #     ion.calculate_from_energy_mev(ion._energy_mev + dE / 1E6)
+    #     return ion.v_m_per_s() * v / initial_v
 
-        dE = 0.0
-        ttf = 1.0
-
-        for segment in self._segments:
-            if segment.check_intersection(rc[:2], rd[:2]):
-                print('Intersection')
-                dE += ion.q() * np.sin(2 * np.pi * self.rf_freq * t + self.rf_phase + segment.phase) * self.v_peak * ttf
-                print(np.mod(np.rad2deg(2 * np.pi * self.rf_freq * t + self.rf_phase), 360))
-
-        # if not intersected:
-        #     return v
-        # dE = ion.q() * np.sin( 2 * np.pi * self.rf_freq * t + self.rf_phase) * self.v_peak * ttf
-        # print("Gained energy {:.3f} keV".format(dE / 1E3))
-        ion.calculate_from_energy_mev(ion._energy_mev + dE / 1E6)
-        return ion.v_m_per_s() * v / initial_v
-
-    def initialize(self):
-        trj_design = self._spiral_inflector.analytic_variables["trj_design"]
-        v_design = self._spiral_inflector.analytic_variables["v_design"]
-        self._xi, self._vi = trj_design[-1, :], v_design[-1, :]
-
-    def create_initial_dees(self):
-
-        thetas = np.linspace(0, 7 * np.pi / 4, 8) + np.pi / 8.0
-        r_init, r_final = 0.05, 1
-
-        ra = np.array([r_init * np.cos(thetas), r_init * np.sin(thetas)])
-        rb = np.array([r_final * np.cos(thetas), r_final * np.sin(thetas)])
-
-        self._segments.append(CRSegment(ra=ra[:, 0], rb=rb[:, 0], phase=np.pi))
-        self._segments.append(CRSegment(ra=ra[:, 1], rb=rb[:, 1], phase=0))
-        self._segments.append(CRSegment(ra=ra[:, 2], rb=rb[:, 2], phase=np.pi))
-        self._segments.append(CRSegment(ra=ra[:, 3], rb=rb[:, 3], phase=0))
-        self._segments.append(CRSegment(ra=ra[:, 4], rb=rb[:, 4], phase=np.pi))
-        self._segments.append(CRSegment(ra=ra[:, 5], rb=rb[:, 5], phase=0))
-        self._segments.append(CRSegment(ra=ra[:, 6], rb=rb[:, 6], phase=np.pi))
-        self._segments.append(CRSegment(ra=ra[:, 7], rb=rb[:, 7], phase=0))
+    def initialize(self, xi=None, vi=None):
+        if xi is None and vi is None:
+            trj_design = self._spiral_inflector.analytic_variables["trj_design"]
+            v_design = self._spiral_inflector.analytic_variables["v_design"]
+            self._xi, self._vi = trj_design[-1, :], v_design[-1, :]
+        else:
+            self._xi, self._vi = xi, vi
 
     def set_inflector(self, spiral_inflector):
         self._spiral_inflector = spiral_inflector
@@ -178,36 +160,24 @@ class CentralRegion(object):
         if self._initialized:
             self.initialize()
 
-    def plot_segments(self):
+    def add_dee(self, dee):
+        self._dees.append(dee)
+
+        return 1
+
+    def plot_dees(self):
         fig = plt.figure()
         ax = fig.add_subplot(111)
 
-        for seg in self._segments:
-            ax.plot([seg.ra[0], seg.rb[0]], [seg.ra[1], seg.rb[1]], color=colors[0])
-            ax.scatter(seg.ra[0], seg.ra[1], marker='o', color=colors[0])
-            ax.scatter(seg.rb[0], seg.rb[1], marker='o', color=colors[0])
-            # e_ang = np.arctan(seg.ehat[1] / seg.ehat[0])
-            # xl, yl = 0.005 * np.cos(e_ang), 0.02 * np.sin(e_ang)
-            # ax.plot([seg.mid[0], seg.mid[0] + seg.ehat[0] * xl], [seg.mid[1], seg.mid[1] + seg.ehat[1] * yl])
+        for dee in self._dees:
+            dee.plot_segments(show=False, ax=ax)
 
         ax.grid(True)
-        ax.set_xlim([-0.25, 0.25])
-        ax.set_ylim([-0.25, 0.25])
+        ax.set_xlim([-self.r_cr, self.r_cr])
+        ax.set_ylim([-self.r_cr, self.r_cr])
         ax.set_aspect(1)
         ax.set_xlabel('x (m)')
         ax.set_ylabel('y (m)')
-
-        si = self._spiral_inflector
-
-        trj_design = si.analytic_variables["trj_design"]
-        ax.plot(trj_design[:, 0], trj_design[:, 1], color=colors[1])
-
-        xc, yc = si.calculate_orbit_center()
-        ax.scatter(xc, yc, marker='X', color=colors[2])
-
-        for tracked_trj in self._tracked_trjs:
-            ax.plot(tracked_trj[:, 0], tracked_trj[:, 1], color=colors[3])
-
         plt.show()
 
     def plot_bfield(self, nx=100, ny=100):
@@ -238,26 +208,187 @@ class CentralRegion(object):
         return r, v
 
 
-rot90 = np.array([[np.cos(np.pi / 2), -np.sin(np.pi / 2), 0.0],
-                  [np.sin(np.pi / 2), np.cos(np.pi / 2), 0.0],
-                  [0.0, 0.0, 1.0]])
+class Dee(object):
+
+    def __init__(self):
+
+        self.meeting_point = [0.0, 0.0]
+        self.opening_angle = 45.0
+        self._opening_angle = np.deg2rad(self.opening_angle)
+        self.angle = 0.0
+
+        self._top_segments, self._bottom_segments = [], []
+        self._char_len = 0.05  # Characteristic length of CRSegments
+
+    def rotate(self, angle, angle_unit="deg"):
+        """
+        Rotates the entire Dee object by angle
+        :param angle:
+        :return:
+        """
+        self.angle = angle
+
+        for seg in self._top_segments + self._bottom_segments:
+            seg.rotate(angle, angle_unit=angle_unit)
+
+        return 0
+
+    def initialize(self):
+
+        # Initial top segment
+        ra = self._char_len * np.array([np.cos(self._opening_angle / 2.0),
+                                        np.sin(self._opening_angle / 2.0)])
+
+        rb = 2 * self._char_len * np.array([np.cos(self._opening_angle / 2.0),
+                                            np.sin(self._opening_angle / 2.0)])
+
+        top_seg = CRSegment(ra, rb)
+
+        # Initial bottom segment
+        ra = self._char_len * np.array([np.cos(-self._opening_angle / 2.0),
+                                        np.sin(-self._opening_angle / 2.0)])
+
+        rb = 2 * self._char_len * np.array([np.cos(-self._opening_angle / 2.0),
+                                            np.sin(-self._opening_angle / 2.0)])
+
+        bottom_seg = CRSegment(ra, rb)
+
+        self._top_segments.append(top_seg)
+        self._bottom_segments.append(bottom_seg)
+
+        pass
+
+    def next_bottom_segment(self, angle_offset=0.0, length=None, angle_unit="deg"):
+
+        if angle_unit == "deg":
+            _angle = np.deg2rad(angle_offset)
+        else:
+            _angle = angle_offset
+
+        if length is None:
+            length = self._char_len
+
+        ra = np.array([0.0, 0.0])
+        rb = length * np.array([np.cos(_angle), np.sin(_angle)])
+
+        prev_seg = self._bottom_segments[-1]
+        next_seg = CRSegment(ra, rb, color=1)
+
+        next_seg.rotate(self.angle - self.opening_angle / 2.0, angle_unit="deg")
+        next_seg.translate(prev_seg.rb)
+
+        self._bottom_segments.append(next_seg)
+
+        return 0
+
+    def next_top_segment(self, angle_offset=0.0, length=None, angle_unit="deg"):
+
+        if angle_unit == "deg":
+            _angle = np.deg2rad(angle_offset)
+        else:
+            _angle = angle_offset
+
+        if length is None:
+            length = self._char_len
+
+        ra = np.array([0.0, 0.0])
+        rb = length * np.array([np.cos(_angle), np.sin(_angle)])
+
+        prev_seg = self._top_segments[-1]
+        next_seg = CRSegment(ra, rb, color=1)
+
+        next_seg.rotate(self.angle + self.opening_angle / 2.0, angle_unit="deg")
+        next_seg.translate(prev_seg.rb)
+
+        self._top_segments.append(next_seg)
+
+        return 0
+
+    def split(self):
+        pass
+
+    # def add_seg(self, seg, dummy=False):
+    #     if dummy:
+    #         self.dummy_dee_segs.append(seg)
+    #     else:
+    #         self.dee_segs.append(seg)
+    #
+    #     return 0
+
+    def set_meeting_point(self, point):
+        self.meeting_point = point
+
+        return 0
+
+    def plot_segments(self, show=True, ax=None):
+
+        if show:
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+
+        for seg in self._top_segments:
+            ax.plot([seg.ra[0], seg.rb[0]], [seg.ra[1], seg.rb[1]], color=colors[seg.color])
+            ax.scatter(seg.ra[0], seg.ra[1], marker='o', color=colors[0])
+            ax.scatter(seg.rb[0], seg.rb[1], marker='o', color=colors[0])
+
+        for seg in self._bottom_segments:
+            ax.plot([seg.ra[0], seg.rb[0]], [seg.ra[1], seg.rb[1]], color=colors[seg.color])
+            ax.scatter(seg.ra[0], seg.ra[1], marker='o', color=colors[0])
+            ax.scatter(seg.rb[0], seg.rb[1], marker='o', color=colors[0])
+
+        if show:
+            ax.grid(True)
+            ax.set_xlim([-0.25, 0.25])
+            ax.set_ylim([-0.25, 0.25])
+            ax.set_aspect(1)
+            ax.set_xlabel('x (m)')
+            ax.set_ylabel('y (m)')
+            plt.show()
+
+        return 0
 
 
 class CRSegment(object):
-    def __init__(self, ra, rb, phase):
-        self.ra = ra  # Inner mid-theta coordinate (x,y)
-        self.rb = rb  # Outer mid-theta coordinate (x,y)
+    def __init__(self, ra, rb, color=0):
+        self.ra = ra  # Inner coordinate (x,y)
+        self.rb = rb  # Outer coordinate (x,y)
         self.mid = 0.5 * (ra + rb)
-        self.ehat = None
-        self.phase = phase
 
-    def get_field(self):
-        self.ehat = rot90.dot(
-            np.array([self.ra[0], self.ra[1], 1.0]) / np.linalg.norm(np.array([self.ra[0], self.ra[1], 1.0])))[:2]
-        self.ehat /= np.linalg.norm(self.ehat)
-        return self.ehat
+        self.color = color
+
+    def rotate(self, angle, angle_unit="deg"):
+
+        if angle_unit == "deg":
+            _angle = np.deg2rad(angle)
+        elif angle_unit == "rad":
+            _angle = angle
+        else:
+            return 1
+
+        rot = np.array([[np.cos(_angle), -np.sin(_angle), 0.0],
+                        [np.sin(_angle), np.cos(_angle), 0.0],
+                        [0.0, 0.0, 1.0]])
+
+        _ra, _rb = np.array([self.ra[0], self.ra[1], 1.0]), np.array([self.rb[0], self.rb[1], 1.0])
+
+        new_ra = np.matmul(rot, _ra[np.newaxis].T)[:2, 0]
+        new_rb = np.matmul(rot, _rb[np.newaxis].T)[:2, 0]
+
+        self.ra = new_ra
+        self.rb = new_rb
+        self.mid = 0.5 * (new_ra + new_rb)
+
+        return 0
+
+    def translate(self, dr):
+
+        self.ra = self.ra + dr
+        self.rb = self.rb + dr
+
+        return 0
 
     def check_intersection(self, rc, rd):
+
         return check_intersection(self.ra, self.rb, rc, rd)
 
 
