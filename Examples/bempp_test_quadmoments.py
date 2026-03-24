6c6d6bf95603f2b2d6260e098393e8c473602a9b
MoonMan2610 <167229980+MoonMan2610@users.noreply.github.com>
2026-03-24 08:24:39 -0600

    Add files via upload

---
+++ Examples/bempp_test_quadmoments.py
@@ -0,0 +1,154 @@
+from spyral_inflector import *
+
+import os
+os.environ["PYOPENCL_COMILER_OUTPUT"] = "1"
+os.environ["PYOPENCL_CT"] = "1:0"
+
+
+
+##- Injection parameters -##
+MEAN_INJECTION_ENERGY = 0.07 # 70 keV out of RFQ
+ION_SPECIES           = "H2_1+"
+
+##- Spiral Inflector Parameters -##
+METHOD       = "numerical"  
+SOLVER       = "bempp"     
+VOLTAGE      = 12000.0      # Voltage applied across two spiral electrodes, 12000 default
+PLATE_GAP    = 0.018        # Gap between electrodes [m], 0.018 default
+TILT         = 27.0         # Angle of the exit of the spiral inflector [degrees], 27 default
+THICKNESS    = 0.01         # Electrode thickness [m], 0.01 default
+V_SHAPE      = 0.00015      # Characterizes the V shape of the electrodes [m], 0.0015 default
+SIM_POINTS   = 100          # Points along the beam trajectory, controls the resolution of the solution
+ASPECT_RATIO = 2.5	    # Controls width of electrodes relative to thickness
+ROTATION     = 0.0          # Rotation [deg] of the inflector
+GAMMA        = 15.0         # Wedge cut angle [deg]
+ANGLING      = 10.0	    # Plate face angling [deg], deviation from parallel. Will vary from -ANGLING to +ANGLING.
+DEBUG        = True
+
+
+h2p = ParticleDistribution(species=IonSpecies(ION_SPECIES))
+h2p.set_mean_energy_z_mev(MEAN_INJECTION_ENERGY)
+
+si = SpiralInflector(ion           = H2P,
+                     method        = METHOD,
+                     volt          = VOLTAGE,
+                     gap           = PLATE_GAP,
+                     tilt          = TILT,
+                     dx            = THICKNESS,
+                     sigma         = V_SHAPE,
+                     ns            = SIM_POINTS,
+                     aspect_ratio  = ASPECT_RATIO,
+                     rotation      = ROTATION,
+                     debug         = DEBUG,
+                     gammaAng      = GAMMA,
+                     anglingAng    = ANGLING)
+
+
+
+# si.load_bfield()
+si.load_bfield(bfield=Field(dim=0, field={"x": 0.0, "y": 0.0, "z": -1.04}))
+
+si.initialize()
+
+# si.generate_geometry()
+# draw_geometry(si, freq=10, show=True)
+
+si.set_parameter(key="h", value=0.005)  # Mesh characteristic length
+si.set_parameter(key="make_aperture", value=True)
+si.set_parameter(key="aperture_params", value={"thickness": 4e-3,
+                                               "radius": 50e-3,
+                                               "length": 45e-3,
+                                               "width": 18e-3,
+                                               "top_distance": 5e-3,
+                                               "bottom_distance": 10e-3,
+                                               "hole_type": "rectangle",
+                                               "voltage": 0.0})
+
+si.set_parameter(key="make_cylinder", value=False)
+si.set_parameter(key="cylinder_params", value={"radius": 120e-3,
+                                               "zmin": -250e-3,
+                                               "zmax": 80e-3,
+                                               "voltage": 0.0})
+
+si.set_parameter(key="make_housing",
+                 value=True)
+si.set_parameter(key="housing_params",
+                 value={"zmin": -0.12,
+                        "zmax": 0.03,
+                        "span": True,
+                        "gap": 10E-3,
+                        "thickness": 5E-3,
+                        "voltage": 0.0,
+                        "experimental": True})
+
+
+# Optional, can create quadrupoles
+# a,b control shape of hyperbolic electrodes, r the outer radius.
+# a,b,r are fixed for all quads. Different geometry not currently supported
+# z starts takes a list of positions along the z-axis where quads start
+# lengths takes a list of quadrupole lengths along the z-axis
+# voltages takes a list at which each quadrupole operates
+#
+#si.set_parameter(key="quadrupole_params",
+#                 value={ "a": 0.01,
+#                         "b": 0.01,
+#                         "radius": 0.02,
+#                         "z_starts": [-0.31,-0.23],
+#                         "lengths": [0.05,0.05],
+#                         "voltages": [0,0],
+#			 "aper_rad": 0.02})
+
+
+si.generate_geometry()
+si.generate_meshed_model()
+
+ts = time.time()
+si.solve(use_gpu=True)
+print("Solving took {:.4f} s".format(time.time() - ts))
+
+# ts = time.time()
+# si.optimize_fringe(maxiter=2, tol=0.02, res=0.005)
+# print("Optimizing took {:.4f} s".format(time.time() - ts))
+
+ts = time.time()
+print("Calculating electric potential/field...")
+si.calculate_potential(res=0.005,
+                       limits=((-0.08, 0.08), (-0.08, 0.08), (-0.12, 0.05)),
+                       domain_decomp=(3, 3, 3))
+si.calculate_efield()
+print("Calculating field took {:.4f} s".format(time.time() - ts))
+
+ts = time.time()
+bunch = ParticleDistribution.generate_distribution(IonSpecies("H2_1+"),
+                                                   type=['gaussian', 'gaussian', 'gaussian'],
+                                                   s_direction='z',
+                                                   n_particles=200,
+                                                   correlation_matrix=np.eye(6),
+                                                   sigma_x=2e-3,
+                                                   sigma_px=1e-20,
+                                                   sigma_y=2e-3,
+                                                   sigma_py=1e-20,
+                                                   sigma_z=4e-3,
+                                                   sigma_pz=1e-20,
+                                                   cutoff_x=3,
+                                                   cutoff_px=3,
+                                                   cutoff_y=3,
+                                                   cutoff_py=3,
+                                                   cutoff_z=3,
+                                                   cutoff_pz=3
+                                                   )
+
+bunch.set_centroid(0.0, 0.0, -0.13)
+bunch.set_mean_energy_z_mev(0.07)
+
+r, v, active = si.fast_track_batch_with_termination(r_start=bunch.x_vec,
+                                                    v_start=bunch.v_vec,
+                                                    nsteps=1500,
+                                                    dt=1e-10)
+print("Tracking took {:.4f} s".format(time.time() - ts))
+
+r = np.swapaxes(r, 0, 1)
+si.draw_geometry(freq=50, show=True, aux_trajectories=r)
+
+# print(si.electrode_geometry_macro())
+# print(si.aperture_geometry_macro())
